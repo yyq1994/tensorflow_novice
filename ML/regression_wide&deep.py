d25cd453bc68004a731f9c73f6959840173bd9c2
Glare <yyq1004856158@163.com>
2020-02-24 22:48:53 +0800

    Update regression_wide&deep.py

--- ML/regression_wide&deep.py
+++ ML/regression_wide&deep.py
@@ -4,9 +4,12 @@ import pandas as pd
 import sklearn
 import os
 import tensorflow.keras as keras
+
+# 下载数据----------加利福尼亚房价
 from sklearn.datasets import fetch_california_housing
 housing = fetch_california_housing()
 
+# 分割数据
 from sklearn.model_selection import train_test_split
 x_train_all,x_test,y_train_all,y_test =train_test_split(housing.data,housing.target,random_state=7)
 x_train,x_valid,y_train,y_valid = train_test_split(x_train_all,y_train_all,random_state=11)
@@ -18,7 +21,7 @@ x_train_scaled = scaler.fit_transform(x_train)
 x_valid_scaled = scaler.transform(x_valid)
 x_test_scaled = scaler.transform(x_test)
 
-# 函数式API
+#网络构建    函数式API
 input = keras.layers.Input(shape=x_train.shape[1:])
 # deep
 hidden1 = keras.layers.Dense(30,activation='relu')(input)
@@ -29,13 +32,17 @@ output = keras.layers.Dense(1)(concat)
 
 model = keras.models.Model(inputs= [input],outputs =[output])
 
-# 网络结构
+# 网络结构----不一样的
 # model.summary()
 
-model.compile(loss='mean_squared_error',optimizer='adam')
+# 模型编译
+model.compile(loss='mean_squared_error',optimizer='adam')      #optimizer我用了 'sgd'几个epochloss就变成了nan了
 callbacks = [keras.callbacks.EarlyStopping(patience=5,min_delta=1e-2)]
+
+#模型训练
 history = model.fit(x_train_scaled,y_train,validation_data=(x_valid_scaled,y_valid),epochs=100,callbacks=callbacks)
 
+# 效果绘制
 def plot_learning_curves(history):
     pd.DataFrame(history.history).plot(figsize=(8,5))
     plt.grid(True)
